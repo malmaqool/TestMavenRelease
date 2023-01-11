de6e15c1ae2cbd1cabf64ab75115e698c6fa946f
malmaqool <108077081+malmaqool@users.noreply.github.com>
2023-01-11 09:38:26 +0100

    Initial commit
    Add a new maven Spring boot application.

---
+++ src/main/java/com/maqool/testmavenrelease/TestMavenReleaseApplication.java
@@ -0,0 +1,13 @@
+package com.maqool.testmavenrelease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMavenReleaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMavenReleaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maqool/testmavenrelease/TestMavenReleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maqool.testmavenrelease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMavenReleaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
